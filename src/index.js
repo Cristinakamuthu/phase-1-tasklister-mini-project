e3312a90a416ec3938b5f2d24a6e445ec73450b2
Cristina <kamuthucristina@gmail.com>
2025-03-19 13:02:23 +0300

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+    form.addEventListener('submit',(e)=> {
+      e.preventDefault();
+      let task = document.querySelector("#new-task-description")
+
+      writeList(task.value)
+      task.value ="  ";
+    })
+
 });
+
+function writeList (todo){
+  let write = document.createElement("p")
+  write.style.color = "green"
+  let btn = document.createElement ("button")
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = "x"
+  btn.style.color = "white"
+  btn.style.backgroundColor = "black"
+  write.textContent = `${todo}`
+  write.appendChild(btn)
+  console.log(write)
+  document.querySelector('#list').appendChild(write)
+}
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
